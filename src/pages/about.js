911f252edea675616b556e176eb9c56d60ecdc4a
ng0cminh <ng0cminh@yahoo.com>
2021-12-05 20:34:44 +0700

    fix

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,14 +1,15 @@
 import Layout from "../components/Layout";
 import { getListNameFolder } from "../lib/posts";
+import { getPageBySlug } from "../lib/pages";
 
-function About({ menu }) {
+function About({ menu, contents }) {
   const metadata = {
     title: "Giới thiệu",
     slug: "about",
   };
   return (
     <Layout metadata={metadata} menu={menu}>
-      <h1>About Page</h1>
+      {contents}
     </Layout>
   );
 }
@@ -17,13 +18,15 @@ export default About;
 
 export async function getStaticProps() {
   // Get external data from the file system, API, DB, etc.
-  const menu = getListNameFolder();
+  const menu = await getListNameFolder();
+  const contents = await getPageBySlug("about");
 
   // The value of the `props` key will be
   // passed to the `Home` component
   return {
     props: {
       menu,
+      contents,
     },
   };
 }
