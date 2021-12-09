fa71430a9680181f524e60ba8d323fce3ce21284
unknown <ng0cminh@yahoo.com>
2021-12-09 09:15:27 +0700

    fix

--- src/components/Sidebar/index.js
+++ src/components/Sidebar/index.js
@@ -1,19 +1,3 @@
-<<<<<<< HEAD
-/* eslint-disable @next/next/no-img-element */
-import FeaturedBox from "../Widgets/FeaturedBox";
-import AboutBox from "../Widgets/AboutBox";
-
-const Sidebar = ({ featuredPosts }) => {
-  return (
-    <aside className="sidebar" id="sidebar">
-      <AboutBox />
-      <FeaturedBox featuredPosts={featuredPosts} />
-    </aside>
-  );
-};
-
-export default Sidebar;
-=======
 /* eslint-disable @next/next/no-img-element */
 import SelectionBox from "../Widgets/SelectionBox";
 import AboutBox from "../Widgets/AboutBox";
@@ -28,4 +12,3 @@ const Sidebar = ({ selectionPosts }) => {
 };
 
 export default Sidebar;
->>>>>>> 308cb2898801bd677b6350d11c37530f5273c1f5
--- src/components/Widgets/SelectionBox.js
+++ src/components/Widgets/SelectionBox.js
@@ -1,52 +1,3 @@
-<<<<<<< HEAD:src/components/Widgets/FeaturedBox.js
-import Link from "next/link";
-import Image from "next/image";
-
-function FeaturedBox({ featuredPosts }) {
-  return (
-    <div className="widget">
-      <h3 className="widget-title">
-        <Link href="/featured-posts">
-          <a>Bài viết nổi bật</a>
-        </Link>
-      </h3>
-      <div className="widget-content">
-        <ul className="widget-post">
-          {featuredPosts.map((post, index) => {
-            return (
-              <li className="item-post" key={post.slug + index}>
-                <span>
-                  <Link href={`/${post.slug}`}>
-                    <a>
-                      <Image
-                        src={
-                          post.image
-                            ? `/images/contents/${post.folder}/${post.image}`
-                            : `/images/default/article.jpg`
-                        }
-                        width={90}
-                        height={60}
-                        alt={post.title ? post.title : `Banner Default`}
-                      />
-                    </a>
-                  </Link>
-                </span>
-                <h4>
-                  <Link href={`/${post.slug}`}>
-                    <a href={`/${post.slug}`}>{post.title}</a>
-                  </Link>
-                </h4>
-              </li>
-            );
-          })}
-        </ul>
-      </div>
-    </div>
-  );
-}
-
-export default FeaturedBox;
-=======
 import Link from "next/link";
 import Image from "next/image";
 
@@ -94,4 +45,3 @@ function SelectionBox({ selectionPosts }) {
 }
 
 export default SelectionBox;
->>>>>>> 308cb2898801bd677b6350d11c37530f5273c1f5:src/components/Widgets/SelectionBox.js
