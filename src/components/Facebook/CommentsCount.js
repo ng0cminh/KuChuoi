2cbcec60704e2caf3c65819448f49322771793a5
unknown <ng0cminh@yahoo.com>
2021-12-29 22:15:23 +0700

    fix

--- src/components/Facebook/CommentsCount.js
+++ src/components/Facebook/CommentsCount.js
@@ -2,7 +2,7 @@ import { FacebookProvider, CommentsCount } from "react-facebook";
 
 const CommentsFacebook = ({ href }) => {
   return (
-    <FacebookProvider appId="636890294324159">
+    <FacebookProvider appId="636890294324159" language="vi_VN">
       <CommentsCount href={href} />
     </FacebookProvider>
   );
